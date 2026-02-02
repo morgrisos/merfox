f480069d11b3a30851dea839d8d12ba7611eb554
MerFox Dev <morgrisos@users.noreply.github.com>
2026-02-02 18:16:18 +0900

    feat: bundle Material Symbols font locally for offline support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,7 @@ export default function Document() {
     return (
         <Html lang="ja">
             <Head>
-                {/* Material Symbols Outlined Font */}
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-                />
+                {/* Fonts loaded locally via @font-face in index.css */}
             </Head>
             <body>
                 <Main />
